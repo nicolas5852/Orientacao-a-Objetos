4147434fdd3c629a8218f745d9f16945efde7578
nicolas5852 <101266003+nicolas5852@users.noreply.github.com>
2026-02-19 20:19:32 -0300

    Add files via upload

---
+++ Aula2/projExemplo3/src/Exemplo3.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author 0030482423014
+ */
+public class Exemplo3 {
+
+    public static void main(String[] args) {
+        int num;
+        
+        num = (int) (Math.random() * 100);
+        
+        System.out.println(num);
+    }
+    
+}
